632e26d5f63cf70a5887b99ca6019e9f5367276d
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-16 04:25:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,8 +48,9 @@ def reduce_to_all_true(source_array)
   while i < source_array.length
     return false if !source_array[i]
     i += 1
+  else
+    true
   end
-  true
 end
 
 def reduce_to_any_true(source_array)
